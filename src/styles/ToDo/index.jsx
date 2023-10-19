7d16eb99eaf72ae10a38f9bdabfbfec443369f65
shreyash19-pixel <shreyashp734@gmail.com>
2023-10-19 19:21:33 +0530

    completed

--- src/styles/ToDo/index.jsx
+++ src/styles/ToDo/index.jsx
@@ -16,6 +16,124 @@ export const ToDoWrap = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    background: linear-gradient(to bottom, #4caf50, #45a049, #388e3c);
+    background-color: #2061CB;
+    padding-inline: 20px;
 
+`
+export const SadIcon = styled.div`
+    font-size: 36px;
+    color: grey;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    @media (max-width: 425px)
+    {
+        font-size: 24px;
+    }
+`
+
+export const EmptyMessageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding-bottom: 65px;
+  @media (max-width: 425px)
+    {
+        gap:7px;
+    }
+`
+
+export const EmptyMessage = styled.h1`
+    color: grey;
+    font-weight: 400;
+
+    @media (max-width: 425px)
+    {
+        font-size: 24px;
+    }
+`
+
+export const TodoList = styled.div`
+    width: 100%;
+    max-width: 500px;
+    min-height: 500px;
+    background-color: white;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+`
+
+export const AddToList = styled.div`
+    width: 100%;
+    display: flex;
+    gap: 15px;
+    padding-inline: 10px;
+`
+
+export const InputBar = styled.input`
+    width: 100%;
+    border-radius: 4px;
+    border: 2px solid #2061CB;
+    padding-top: 3px;
+    padding-left: 4px;
+`
+
+export const AddButton = styled.div`
+    width: 100%;
+    max-width: 80px;
+    min-height: 50px;
+    background-color: #2061CB;
+    color: white;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 4px;
+`
+export const TaskContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+export const TaskWrap = styled.div`
+    width: 100%;
+    height: 100%;
+    max-height: 340px;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    padding-inline: 8px;
+    gap: 10px;
+`
+
+export const Tasks = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    gap: 15px;
+    background-color: #C4E1E5;
+    padding: 10px;
+    border-radius: 4px;
+    border: 1px solid black;
+`
+
+export const TaskName = styled.h3`
+    color: black;
+    font-weight: 500;
+`
+
+export const DeleteIcon = styled.div`
+    font-size: 16px;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    background-color: #1A1BA1;
+    padding: 8px;
+    border-radius: 4px;
 `
--- src/views/ToDo/index.jsx
+++ src/views/ToDo/index.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
-import { ToDoContainer, ToDoWrap } from '../../styles/ToDo'
+import React,{useState,useRef,useEffect} from 'react'
+import { AddButton, AddToList, DeleteIcon, EmptyMessage, EmptyMessageWrapper, InputBar, SadIcon, TaskContainer, TaskName, TaskWrap, Tasks, ToDoContainer, ToDoWrap, TodoList } from '../../styles/ToDo'
+import {AiFillDelete,AiOutlinePlus} from 'react-icons/ai'
+import {BiSad} from 'react-icons/bi'
 
 const ToDo = () => {
+
+   const newTask = JSON.parse(localStorage.getItem('tasks')) || []
+   const [taskArray, setTaskArray] = useState(newTask)
+   const [tasks, setTasks] = useState('')
+   const taskRef = useRef(null)
+
+
+   useEffect(() => {
+    localStorage.setItem('tasks',JSON.stringify(taskArray))
+   },[taskArray])
+
+   const handleTasks = (e) => {
+    setTasks(e.target.value)
+   }
+
+   const handleOutput = () => {
+        setTaskArray([...taskArray,tasks]) 
+        taskRef.current.value = ''
+    }
+
+    const handleDelete = (index) => {
+        const updatedArray = [...taskArray]
+        updatedArray.splice(index,1)
+        setTaskArray(updatedArray)
+    }
+
+
+   
   return (
     <ToDoContainer>
         <ToDoWrap>
-            
+            <TodoList>
+                <AddToList>
+                    <InputBar ref = {taskRef} type = "text" placeholder = "Add tasks from here..." onChange = {handleTasks}/>
+                    <AddButton onClick = {handleOutput}><AiOutlinePlus/></AddButton>
+                </AddToList>
+                <TaskContainer>
+                    {taskArray.length > 0 ?
+                    (<TaskWrap>
+                        {taskArray.map((task,index) => (
+                        <Tasks key = {index}>
+                            <TaskName>{task}</TaskName>
+                            <DeleteIcon onClick = {() => {handleDelete(index)}}><AiFillDelete/></DeleteIcon>
+                        </Tasks>
+                        ))}
+                    </TaskWrap>) :
+                    (
+                        <EmptyMessageWrapper>
+                            <EmptyMessage>No Task Added yet</EmptyMessage>
+                            <SadIcon><BiSad/></SadIcon>
+                        </EmptyMessageWrapper>
+                    )}
+                </TaskContainer>
+            </TodoList>
         </ToDoWrap>
     </ToDoContainer>
   )
